991134f65eda540b298f12a99dc3669fcfafcb63
Bukabuka321 <20uva02@gmail.com>
2023-05-18 23:04:52 +0200

    added new component, installed scss

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,12 @@
 import Contact from "@/components/Contact";
+import Introduction from "@/components/Introduction";
 import LandingPart from "@/components/LandingPart";
 
 export default function Home() {
   return (
     <>
       <LandingPart />
+      <Introduction />
       <Contact />
     </>
   );
