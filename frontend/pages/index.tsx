8d155efe48290868426b78aa431a0e06d8d96979
Bukabuka321 <20uva02@gmail.com>
2023-05-20 21:56:02 +0200

    added new component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,14 @@
 import Contact from "@/components/Contact";
 import Introduction from "@/components/Introduction";
 import LandingPart from "@/components/LandingPart";
+import Tasks from "@/components/Tasks";
 
 export default function Home() {
   return (
     <>
       <LandingPart />
       <Introduction />
+      <Tasks />
       <Contact />
     </>
   );
